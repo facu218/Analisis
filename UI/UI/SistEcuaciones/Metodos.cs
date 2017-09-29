ef99b257d2c01f3ef85e842022381b371348974d
joaribero <joaribero_la12@hotmail.com>
2017-09-28 23:35:07 -0300

    Correcion de funcion
    Sigue sin andar, corrigiendo mentalmente posibles errores. O quizá rompiendo lo que ya ande. Continua la paradoja.

--- UI/UI/SistEcuaciones/Metodos.cs
+++ UI/UI/SistEcuaciones/Metodos.cs
@@ -58,9 +58,9 @@ namespace UI.SistEcuaciones
                 {
                     for (int k=0; k<variables; k++)
                     {
+                        temp[i] = matriz[k, Col] * matriz[Fil, i];           // ARMA ARRAy
                         if (k > Fil | k<Fil)
-                        {
-                            temp[i] = matriz[k, Col] * matriz[Fil, i];     // ARMA ARRAy
+                        {                               
                             matriz[k, Col] = matriz[k, i] - temp[i];       // Realiza la op. con las filas sig.
                         }                           
                     }
