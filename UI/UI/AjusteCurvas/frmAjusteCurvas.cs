1960810ae8a09d3db8aca49c0f0c515b58027d58
facu218 <facucai_95@hotmail.com>
2017-11-03 07:29:30 -0300

    Correccion ingreso de decimales

--- UI/UI/AjusteCurvas/frmAjusteCurvas.cs
+++ UI/UI/AjusteCurvas/frmAjusteCurvas.cs
@@ -27,17 +27,17 @@ namespace UI.Regresion
         private void button2_Click(object sender, EventArgs e)
         {
             int n = Convert.ToInt32(textBox1.Text);
-            int XY=0, sx=0 ,sy=0, sx2 = 0;
+            double XY=0, sx=0 ,sy=0, sx2 = 0;
 
             //Calculo de A1
             
             
             for (int i = 0; i < n; i++)
             {
-                XY = XY + Convert.ToInt32(dgvPuntos[0, i].Value) * Convert.ToInt32(dgvPuntos[1, i].Value);
-                sx = sx + Convert.ToInt32(dgvPuntos[0, i].Value);
-                sy = sy + Convert.ToInt32(dgvPuntos[1, i].Value);
-                sx2 = sx2 + (Convert.ToInt32(dgvPuntos[0, i].Value)) * (Convert.ToInt32(dgvPuntos[0, i].Value));
+                XY = XY + Convert.ToDouble(dgvPuntos[0, i].Value) * Convert.ToDouble(dgvPuntos[1, i].Value);
+                sx = sx + Convert.ToDouble(dgvPuntos[0, i].Value);
+                sy = sy + Convert.ToDouble(dgvPuntos[1, i].Value);
+                sx2 = sx2 + (Convert.ToDouble(dgvPuntos[0, i].Value)) * (Convert.ToDouble(dgvPuntos[0, i].Value));
             }
 
             double A1 = ((n * XY) - (sx * sy)) / ((n * sx2) - (sx*sx));
@@ -46,15 +46,15 @@ namespace UI.Regresion
             double sr = 0, st = 0;
             for (int i=0; i<n; i++)
             {
-                int xi = Convert.ToInt32(dgvPuntos[0, i].Value);
-                int yi = Convert.ToInt32(dgvPuntos[1, i].Value);
+                double xi = Convert.ToDouble(dgvPuntos[0, i].Value);
+                double yi = Convert.ToDouble(dgvPuntos[1, i].Value);
                 double sri = ((A1 * xi) + A0);
                 sr = sr + Math.Abs(Math.Pow(yi-sri,2));
-                st = st + Math.Abs(Convert.ToInt32(dgvPuntos[1, i].Value) - recP);
+                st = st + Math.Abs(Convert.ToDouble(dgvPuntos[1, i].Value) - recP);
             }
             double r = Math.Sqrt((st - sr) / st)*100;
 
-            textBox2.Text = "Y = " + A1 + "X +" + A0;
+            textBox2.Text = "Y = " + A1 + "X + (" + A0+ ")";
             txtCoe.Text = "%" + r;
         }
 
